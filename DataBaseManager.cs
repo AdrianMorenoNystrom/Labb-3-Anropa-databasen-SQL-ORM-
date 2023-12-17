7a0edea82841a24c3db69a702f14f8b34bba1638
AdrianMorenoNystrom <adrianmorenonystrom@gmail.com>
2023-12-17 10:30:20 +0100

    .

--- DataBaseManager.cs
+++ DataBaseManager.cs
@@ -19,6 +19,7 @@ namespace LABB3
             dbContext = new SchoolContext();
             dbContext.Database.EnsureCreated();
         }
+        //Skapa personal och lägga till dom i databasen
         public void createPersonal()
         {
             Console.WriteLine("Skriv in information om den anställde:");
@@ -61,153 +62,198 @@ namespace LABB3
             }
         }
 
-            public void createStudent()
+        //Skapa elev och lägga till i databasen
+        public void createStudent()
+        {
+            Console.WriteLine("Skriv in information om eleven:");
+
+            Console.Write("ElevID: ");
+            int studentId = int.Parse(Console.ReadLine());
+
+            Console.Write("Förnamn: ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("Efternamn: ");
+            string lastName = Console.ReadLine();
+
+            string personalNumber;
+
+            while (true)
             {
-                Console.WriteLine("Skriv in information om eleven:");
+                Console.Write("Personnummer (ÅÅMMDD-XXXX): ");
+                personalNumber = Console.ReadLine();
 
-                Console.Write("ElevID: ");
-                int studentId = int.Parse(Console.ReadLine());
-
-                Console.Write("Förnamn: ");
-                string firstName = Console.ReadLine();
-
-                Console.Write("Efternamn: ");
-                string lastName = Console.ReadLine();
-
-                string personalNumber;
-
-                while (true)
+                if (ValidatePersonalNumber(personalNumber))
                 {
-                    Console.Write("Personnummer (ÅÅMMDD-XXXX): ");
-                    personalNumber = Console.ReadLine();
-
-                    if (ValidatePersonalNumber(personalNumber))
+                    var newStudent = new StudentTabell
                     {
-                        var newStudent = new StudentTabell
-                        {
-                            StudentIdPk = studentId,
-                            FörNamn = firstName,
-                            EfterNamn = lastName,
-                            Personnummer = personalNumber
-                        };
+                        StudentIdPk = studentId,
+                        FörNamn = firstName,
+                        EfterNamn = lastName,
+                        Personnummer = personalNumber
+                    };
 
-                        // Add the new student to the database
-                        dbContext.StudentTabells.Add(newStudent);
-                        dbContext.SaveChanges();
+                    // Add the new student to the database
+                    dbContext.StudentTabells.Add(newStudent);
+                    dbContext.SaveChanges();
 
-                        Console.WriteLine("Elev tillagd i databasen");
+                    Console.WriteLine("Elev tillagd i databasen");
 
-                        // Exit the loop since a valid personal number was provided
-                        break;
-                    }
-
+                    // Exit the loop since a valid personal number was provided
+                    break;
                 }
 
             }
 
-            public bool ValidatePersonalNumber(string personalNumber)
-            {
-                // Check if the length is correct
-                if (personalNumber.Length != 11)
-                {
-                    Console.WriteLine("Ogiltig längd på personumret");
-                    return false;
-                }
+        }
 
-                // Check if the format is correct
-                if (!personalNumber.Substring(6, 1).Equals("-"))
-                {
-                    Console.WriteLine("Ogiltigt format på personummret.");
-                    return false;
-                }
-                // The personal number is valid
-                return true;
+        //metod som Validerar personnumret och säkerställer att formatet är korrekt
+        public bool ValidatePersonalNumber(string personalNumber)
+        {
+            // Check if the length is correct
+            if (personalNumber.Length != 11)
+            {
+                Console.WriteLine("Ogiltig längd på personumret");
+                return false;
             }
 
-            public void GetGradesAndCourses()
+            // Check if the format is correct
+            if (!personalNumber.Substring(6, 1).Equals("-"))
             {
-                var courseStatistics = dbContext.KursTabells
-                .Select(course => new
-                {
-                    CourseName = course.KursNamn,
-                    Grades = dbContext.BetygTabells
-                .Where(grade => grade.KursIdFk == course.KursIdPk)
-                .Select(grade => GradeToNumeric(grade.Betyg))
-                .AsEnumerable()
-                })
-                .ToList();
-
-                Console.ForegroundColor = ConsoleColor.DarkRed;
-                Console.WriteLine("Lista över kurser med statistik:");
-                Console.ResetColor();
-                foreach (var statistics in courseStatistics)
-                {
-                    var gradesList = statistics.Grades.ToList();
-
-                    var averageGrade = gradesList.DefaultIfEmpty(0).Average();
-                    var maxGrade = gradesList.DefaultIfEmpty(0).Max();
-                    var minGrade = gradesList.DefaultIfEmpty(0).Min();
-
-                    Console.WriteLine($"Kurs: {statistics.CourseName}, Snittbetyg: {averageGrade}, Högsta betyg: {maxGrade}, Lägsta betyg: {minGrade}");
-                }
+                Console.WriteLine("Ogiltigt format på personummret.");
+                return false;
             }
+            // The personal number is valid
+            return true;
+        }
 
-            private static int GradeToNumeric(string gradeChar)
+        //Hämtar betyg och kurser, samt statistik över dom.
+        public void GetGradesAndCourses()
+        {
+            var courseStatistics = dbContext.KursTabells
+            .Select(course => new
             {
-                return gradeChar.Trim() switch
-                {
-                    "A" => (int)6,
-                    "B" => (int)5,
-                    "C" => (int)4,
-                    "D" => (int)3,
-                    "E" => (int)2,
-                    "F" => (int)1,
-                };
-            }
-
-            public void GetGrades()
-            {
-
-                DateTime currentDate = DateTime.Now;
-
-                DateTime lastMonth = currentDate.AddMonths(-1);
-
-                var grades = dbContext.BetygTabells.ToList()
-                .Join(
-                dbContext.StudentTabells,
-                grade => grade.StudentIdFk,
-                student => student.StudentIdPk,
-                (grade, student) => new { grade, student }
-
-            )
-                .Join(
-                dbContext.KursTabells,
-                combined => combined.grade.KursIdFk,
-                course => course.KursIdPk,
-                (combined, courses) => new { combined.grade, combined.student, courses }
-            )
+                CourseName = course.KursNamn,
+                Grades = dbContext.BetygTabells
+            .Where(grade => grade.KursIdFk == course.KursIdPk)
+            .Select(grade => GradeToNumeric(grade.Betyg))
+            .AsEnumerable()
+            })
             .ToList();
 
-
-                foreach (var grade in grades)
-                {
-                    if (grade.grade.BetygDatum >= lastMonth && grade.grade.BetygDatum <= currentDate)
-                    {
-                        string formattedDate = grade.grade.BetygDatum?.ToString("yyyy-MM-dd");
-                        Console.WriteLine($"\nNamn:{grade.student.FörNamn}{grade.student.EfterNamn} \nKurs:{grade.courses.KursNamn} \nBetyg:{grade.grade.Betyg} \nBetygsättningsdatum:{formattedDate}");
-                    }
-                }
-
-                string currentdate = currentDate.ToString("yyyy-MM-dd");
-                Console.ForegroundColor = ConsoleColor.DarkRed;
-                Console.WriteLine($"Dagens datum: {currentdate}");
-                Console.ResetColor();
-            }
-            //Hämtar alla elever och en switch med olika utfall beroende på vad användaren väljer.
-            public void GetStudents(string sortBy, bool ascending)
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Lista över kurser med statistik:");
+            Console.ResetColor();
+            foreach (var statistics in courseStatistics)
             {
-                //var students = dbContext.StudentTabells.OrderByDescending(x=>x.FörNamn).ToList();
+                var gradesList = statistics.Grades.ToList();
 
+                var averageGrade = gradesList.DefaultIfEmpty(0).Average();
+                var maxGrade = gradesList.DefaultIfEmpty(0).Max();
+                var minGrade = gradesList.DefaultIfEmpty(0).Min();
+
+                Console.WriteLine($"Kurs: {statistics.CourseName}, Snittbetyg: {averageGrade}, Högsta betyg: {maxGrade}, Lägsta betyg: {minGrade}");
+            }
+            Console.WriteLine("\nSiffrorna representerar betygen\n" +
+                        "A=6\n" +
+                        "B=5\n" +
+                        "C=4\n" +
+                        "D=3\n" +
+                        "E=2\n" +
+                        "F=1");
+        }
+
+        //Metod som omvandlar betygen från bokstäver till siffror.
+        private static int GradeToNumeric(string gradeChar)
+        {
+            return gradeChar.Trim() switch
+            {
+                "A" => (int)6,
+                "B" => (int)5,
+                "C" => (int)4,
+                "D" => (int)3,
+                "E" => (int)2,
+                "F" => (int)1,
+            };
+        }
+
+        //Hämtar betyg samt datum när dom sattes. 
+        public void GetGrades()
+        {
+
+            DateTime currentDate = DateTime.Now;
+
+            DateTime lastMonth = currentDate.AddMonths(-1);
+
+            var grades = dbContext.BetygTabells.ToList()
+            .Join(
+            dbContext.StudentTabells,
+            grade => grade.StudentIdFk,
+            student => student.StudentIdPk,
+            (grade, student) => new { grade, student }
+
+        )
+            .Join(
+            dbContext.KursTabells,
+            combined => combined.grade.KursIdFk,
+            course => course.KursIdPk,
+            (combined, courses) => new { combined.grade, combined.student, courses }
+        )
+        .ToList();
+
+
+            foreach (var grade in grades)
+            {
+                if (grade.grade.BetygDatum >= lastMonth && grade.grade.BetygDatum <= currentDate)
+                {
+                    string formattedDate = grade.grade.BetygDatum?.ToString("yyyy-MM-dd");
+                    Console.WriteLine($"\nNamn:{grade.student.FörNamn}{grade.student.EfterNamn} \nKurs:{grade.courses.KursNamn} \nBetyg:{grade.grade.Betyg} \nBetygsättningsdatum:{formattedDate}");
+                }
+            }
+
+            string currentdate = currentDate.ToString("yyyy-MM-dd");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine($"Dagens datum: {currentdate}");
+            Console.ResetColor();
+        }
+
+        //Hämtar alla elever och en switch med olika utfall beroende på vad användaren väljer.
+        public void GetStudents()
+        {
+            Console.WriteLine("1. Förnamn stigande");
+            Console.WriteLine("2. Förnamn fallande");
+            Console.WriteLine("3. Efternamn stigande");
+            Console.WriteLine("4. Efternamn fallande");
+
+            Console.Write("Välj sortering för elever: ");
+            int sortChoice;
+            if (int.TryParse(Console.ReadLine(), out sortChoice))
+            {
+                string sortBy = "lastname";
+                bool ascending = true;
+
+                switch (sortChoice)
+                {
+                    case 1:
+                        sortBy = "firstname";
+                        ascending = true;
+                        break;
+                    case 2:
+                        sortBy = "firstname";
+                        ascending = false;
+                        break;
+                    case 3:
+                        sortBy = "lastname";
+                        ascending = true;
+                        break;
+                    case 4:
+                        sortBy = "lastname";
+                        ascending = false;
+                        break;
+                    default:
+                        Console.WriteLine("Ogiltig inmatning för elevtabell");
+                        break;
+                }
                 var studentsQuery = dbContext.StudentTabells.AsQueryable();
 
                 switch (sortBy)
@@ -231,71 +277,84 @@ namespace LABB3
                     Console.WriteLine($"{student.FörNamn}{student.EfterNamn}");
                 }
             }
-            //Hämtar alla ur personaltabellen oavsett befattning.
-            public void GetEmployees()
+        }
+        //Hämtar alla ur personaltabellen oavsett befattning.
+        public void GetEmployees()
+        {
+            var employees = dbContext.PersonalTabells.ToList();
+
+            Console.WriteLine("Lista över alla anställda:");
+            foreach (var employee in employees)
             {
-                var employees = dbContext.PersonalTabells.ToList();
-
-
-                Console.WriteLine("Lista över alla anställda:");
-                foreach (var employee in employees)
-                {
-                    Console.WriteLine($"{employee.FörNamn} {employee.EfterNamn} - Befattning: {employee.Befattning}");
-                }
-            }
-
-            //Hämtar anställda ur personaltabellen beroende på vilken befattning dom har.
-            public void GetEmployeesByPosition(int positionChoice)
-            {
-
-                if (positionChoice == 8)
-                {
-                    GetEmployees();
-                    return;
-                }
-
-                string position;
-
-                switch (positionChoice)
-                {
-                    case 1:
-                        position = "Lärare";
-                        break;
-                    case 2:
-                        position = "Rektor";
-                        break;
-                    case 3:
-                        position = "Administratör";
-                        break;
-                    case 4:
-                        position = "Vaktmästare";
-                        break;
-                    case 5:
-                        position = "Kioskansvarig";
-                        break;
-                    case 6:
-                        position = "Städare";
-                        break;
-                    case 7:
-                        position = "Ägare";
-                        break;
-                    default:
-                        Console.WriteLine("Ogiltig siffra för befattning.");
-                        return;
-                }
-
-                var employees = dbContext.PersonalTabells
-                    .Where(employees => employees.Befattning == position)
-                    .ToList();
-
-                Console.WriteLine($"\nLista över alla {position.ToLower()}:");
-                foreach (var employee in employees)
-                {
-                    Console.WriteLine($"{employee.FörNamn}{employee.EfterNamn}");
-                }
+                Console.WriteLine($"{employee.FörNamn} {employee.EfterNamn} - Befattning: {employee.Befattning}");
             }
         }
+
+        //Hämtar anställda ur personaltabellen beroende på vilken befattning dom har.
+        public void GetEmployeesByPosition()
+        {
+            Console.WriteLine(
+                           "1.Lärare\n" +
+                           "2.Rektor\n" +
+                           "3.Administratörer\n" +
+                           "4.Vaktmästare\n" +
+                           "5.Kioskansvarig\n" +
+                           "6.Städare\n" +
+                           "7.Ägare\n" +
+                           "8.Alla anställda");
+
+            Console.Write("Välj befattning(1-8): ");
+
+            string position;
+            int positionChoice = int.Parse(Console.ReadLine());
+
+            switch (positionChoice)
+            {
+                case 1:
+                    position = "Lärare";
+                    break;
+                case 2:
+                    position = "Rektor";
+                    break;
+                case 3:
+                    position = "Administratör";
+                    break;
+                case 4:
+                    position = "Vaktmästare";
+                    break;
+                case 5:
+                    position = "Kioskansvarig";
+                    break;
+                case 6:
+                    position = "Städare";
+                    break;
+                case 7:
+                    position = "Ägare";
+                    break;
+                case 8:
+                    GetEmployees();
+                    return;
+                default:
+                    Console.WriteLine("Ogiltig siffra för befattning.");
+                    return;
+            }
+
+            var employees = dbContext.PersonalTabells
+                .Where(employees => employees.Befattning == position)
+                .ToList();
+
+            Console.WriteLine($"\nLista över alla {position.ToLower()}:");
+            foreach (var employee in employees)
+            {
+                Console.WriteLine($"{employee.FörNamn}{employee.EfterNamn}");
+            }
+
+        }
+
+        //Sätt betyg på elevmetod. 
+
     }
+}
 
 
 
--- Menu.cs
+++ Menu.cs
@@ -38,85 +38,16 @@ namespace LABB3
                 switch (choice)
                 {
                     case 1:
-                        Console.WriteLine(
-                            "1.Lärare\n" +
-                            "2.Rektor\n" +
-                            "3.Administratörer\n" +
-                            "4.Vaktmästare\n" +
-                            "5.Kioskansvarig\n" +
-                            "6.Städare\n" +
-                            "7.Ägare\n" +
-                            "8.Alla anställda");
-
-                        Console.Write("Välj befattning(1-8): ");
-                        int positionChoice;
-                        if (int.TryParse(Console.ReadLine(), out positionChoice))
-                        {
-                            database.GetEmployeesByPosition(positionChoice);
-                        }
-                        else
-                        {
-                            Console.WriteLine("Ogiltig inmatning. Ange en siffra.");
-                        }
+                        database.GetEmployeesByPosition();
                         break;
-
                     case 2:
-                        Console.WriteLine("1. Förnamn stigande");
-                        Console.WriteLine("2. Förnamn fallande");
-                        Console.WriteLine("3. Efternamn stigande");
-                        Console.WriteLine("4. Efternamn fallande");
-
-                        Console.Write("Välj sortering för elever: ");
-                        int sortChoice;
-                        if (int.TryParse(Console.ReadLine(), out sortChoice))
-                        {
-                            string sortBy = "lastname";
-                            bool ascending = true;
-
-                            switch (sortChoice)
-                            {
-                                case 1:
-                                    sortBy = "firstname";
-                                    ascending = true;
-                                    break;
-                                case 2:
-                                    sortBy = "firstname";
-                                    ascending = false;
-                                    break;
-                                case 3:
-                                    sortBy = "lastname";
-                                    ascending = true;
-                                    break;
-                                case 4:
-                                    sortBy = "lastname";
-                                    ascending = false;
-                                    break;
-                                default:
-                                    Console.WriteLine("Ogiltig inmatning för elevtabell");
-                                    break;
-                            }
-
-                            database.GetStudents(sortBy, ascending);
-                        }
-                        else
-                        {
-                            Console.WriteLine("Ogiltig inmatning. Ange en siffra.");
-                        }
-                        
+                        database.GetStudents();
                         break;
-
                     case 3:
                         database.GetGrades();
                         break;
                     case 4:
                         database.GetGradesAndCourses();
-                        Console.WriteLine("\nSiffrorna representerar betygen\n" +
-                            "A=6\n" +
-                            "B=5\n" +
-                            "C=4\n" +
-                            "D=3\n" +
-                            "E=2\n" +
-                            "F=1");
                         break;
                     case 5:
                         database.createStudent();
